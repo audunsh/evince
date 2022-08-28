bb8e4863731a0dfdb965ee2d14e8cae7ee69a2cb
Audun Hansen <you@example.com>
2022-08-28 13:59:11 +0200

    cleaned repo

--- js/lib/index.js
+++
@@ -1,7 +0,0 @@
-// Export widget models and views, and the npm package version number.
-//module.exports = require('./example.js');
-//module.exports = require('./example_2.js');
-module.exports = require('./braketview.js');
-//module.exports = require('./braketview_2.js');
-//module.exports = require('./mdview.js');
-module.exports['version'] = '0.23.0'  //require('../package.json').version;
